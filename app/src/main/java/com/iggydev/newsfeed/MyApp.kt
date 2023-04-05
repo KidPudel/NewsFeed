d6386fea1bcc6e3f8a4f955de1bf80b97c8963ba
Iggy Kuperson <I.Kupchinenko@gmail.com>
2023-04-05 17:56:52 +0300

    added all necessary dependencies + configured environment

---
+++ app/src/main/java/com/iggydev/newsfeed/MyApp.kt
@@ -0,0 +1,8 @@
+package com.iggydev.newsfeed
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp: Application() {
+}
---
+++ app/src/main/java/com/iggydev/newsfeed/presentation/views/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.iggydev.newsfeed.presentation.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iggydev.newsfeed.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
